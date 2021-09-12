7e074dcc0538f8cc719adc0f2c187a74828ab19f
Floorss <floor.straver@gmail.com>
2021-09-12 13:19:35 +0200

    Create bayes.py

---
+++ bayes.py
@@ -0,0 +1,7 @@
+class Bayes
+
+def __init__(hypothesis, priors, observations, likelihood):
+    self.hypothesis = hypothesis
+    self.priors = priors
+    self.observations = observations
+    self.likelihood_array = likelihood_array
